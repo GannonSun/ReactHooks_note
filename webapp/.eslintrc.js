031d62a162b0671ef3313c5855c4e6fe2f64b217
GannonSun <gannonsun@gmail.com>
2020-09-02 11:41:21 +0800

    first commit

---
+++ webapp/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: [require.resolve('@umijs/fabric/dist/eslint')],
+  rules: {
+    "no-console": 0
+  },
+  globals: {
+    ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: true,
+    page: true,
+    REACT_APP_ENV: true,
+  },
+};
